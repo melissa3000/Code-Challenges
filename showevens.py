004dcca15ac6b2d73ae8a37ba2304710957ecb4f
Melissa Boyer <melissaboyer3000@gmail.com>
2017-04-05 07:43:13 -0700

    Show even number index HB challenge

---
+++ showevens.py
@@ -0,0 +1,34 @@
+"""Given list of ints, return list of *indices* of even numbers in list.
+
+For example::
+
+    >>> show_evens([])
+    []
+
+    >>> show_evens([2])
+    [0]
+
+    >>> show_evens([1, 2, 3, 4])
+    [1, 3]
+
+"""
+
+
+def show_evens(nums):
+    """Given list of ints, return list of *indices* of even numbers in list."""
+
+    result = []
+
+    if nums == []:
+        return result
+
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            result.append(i)
+    return result
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. EVENLY HANDLED!\n"
