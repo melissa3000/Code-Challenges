d1aeeea05184f2bd92764704894c8abd2d702ade
Melissa <melissaboyer3000@gmail.com>
2017-04-07 07:53:23 -0700

    Balance Paren HB Challenge

---
+++ balancedparentheses.py
@@ -0,0 +1,55 @@
+"""Does a string have balanced parentheses?
+
+For example::
+
+   >>> has_balanced_parens("()")
+   True
+
+   >>> has_balanced_parens("(Oh Noes!)(")
+   False
+
+   >>> has_balanced_parens("((There's a bonus open paren here.)")
+   False
+
+   >>> has_balanced_parens(")")
+   False
+
+   >>> has_balanced_parens("(")
+   False
+
+   >>> has_balanced_parens("(This has (too many closes.) ) )")
+   False
+
+If you receive a string with no parentheses, consider it balanced::
+
+   >>> has_balanced_parens("Hey...there are no parens here!")
+   True
+"""
+
+
+def has_balanced_parens(phrase):
+    """Does a string have balanced parentheses?"""
+
+    paren = []
+
+
+    for char in phrase:
+      if char in "()":
+        paren.append(char)
+
+    if len(paren) == 0:
+      return True
+
+    if len(paren) % 2 != 0:
+      return False
+
+    else:
+      return True
+
+    # print paren
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. YOU CAUGHT ALL THE STRAY PARENS!\n"
