89df44361c9056557a056d3f7b1fc99baa57a5fc
Melissa <melissaboyer3000@gmail.com>
2018-01-31 19:59:53 -0500

    FCC Boolean challenge

---
+++ booWho.js
@@ -0,0 +1,15 @@
+/*
+Check if a value is classified as a boolean primitive. Return true or false.
+
+Boolean primitives are true and false.
+*/
+
+function booWho(bool) {
+  if (bool === true || bool === false) {
+    return true;
+  }
+  return typeof(bool) === true;
+
+}
+
+booWho(true);
