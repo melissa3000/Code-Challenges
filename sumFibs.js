7a174b5940c31eec5801ad0b22b2f1574c152189
Melissa <melissaboyer3000@gmail.com>
2018-02-03 09:19:07 -0500

    FCC Sum Fibs refactored

--- sumFibs.js
+++ sumFibs.js
@@ -10,27 +10,45 @@ For example, sumFibs(10) should return 10 because all odd Fibonacci numbers
 less than 10 are 1, 1, 3, and 5.
 */
 
-function sumFibs(num) {
-  let fibs = [];
-  let x = 1;
-  let y = 1;
-  let z = 0;
-  while (x < num) {
-    fibs.push(x);
-    z = x;
-    x = y;
-    y = z + y;
-  }
 
+function sumFibs(num) {
+  let prevNum = 0;
+  let currentNum = 1;
   let sum = 0;
 
-  for (let i = 0; i < fibs.length; i ++){
-    if (fibs[i] % 2 !== 0 ) {
-      sum += fibs[i];
+  while (currentNum <= num) {
+    if (currentNum % 2 !== 0) {
+      sum += currentNum;
     }
+    currentNum += previousNum;
+    previousNum = currentNum - previousNum;
   }
-  return sum;
 }
 
-sumFibs(75025);
+
+//===========Brute Force==================================================
+
+// function sumFibs(num) {
+//   let fibs = [];
+//   let x = 1;
+//   let y = 1;
+//   let z = 0;
+//   while (x <= num) {
+//     fibs.push(x);
+//     z = x;
+//     x = y;
+//     y = z + y;
+//   }
+
+//   let sum = 0;
+
+//   for (let i = 0; i < fibs.length; i ++){
+//     if (fibs[i] % 2 !== 0 ) {
+//       sum += fibs[i];
+//     }
+//   }
+//   return sum;
+// }
+
+// sumFibs(75025);
 
