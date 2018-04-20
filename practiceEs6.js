bcab04e6b0cff4d7294217517b30420167c326ec
Melissa <melissaboyer3000@gmail.com>
2018-04-20 08:00:02 -0400

    Added Enhanced object literals

--- practiceEs6.js
+++ practiceEs6.js
@@ -813,6 +813,81 @@ profile.getName();
 
 //================================================================
 
+// Enhanced object literals using ES6
+// Refactor into ES6:
+function createBookShop(inventory){
+  return {
+    inventory: inventory,
+    inventoryValue: function() {
+      return this.inventory.reduce((total, book) => total + book.price, 0);
+    },
+    priceForTitle: function(title) {
+      return this.inventory.find(book => book.title === title).price;
+    }
+  };
+}
+
+const inventory = [
+  { title: 'Harry Potter', price: 10 },
+  { title: 'Eloquent Javascript', price: 15 }
+];
+
+const bookShop = createBookShop(inventory);
+bookShop.inventoryValue();
+bookShop.priceForTitle('Harry Potter');
+
+
+
+// Solution:
+
+function createBookShop(inventory) {
+  return {
+    inventory,
+    inventoryValue() {
+      return this.inventory.reduce((total, book) => total + book.price, 0);
+    },
+    priceForTitle(title) {
+      return this.inventory.find(book => book.title === title).price;
+    }
+  };
+}
+
+const inventory = [
+  { title: 'Harry Potter', price: 10 },
+  { title: 'Eloquent Javascript', price: 15 }
+];
+
+const bookShop = createBookShop(inventory);
+bookShop.inventoryValue();
+bookShop.priceForTitle('Harry Potter');
+
+
+//================================================================
+
+function saveFile(url, data) {
+  $.ajax({ method: 'POST', url: url, data: data });
+}
+
+const url = 'http://fileupload.com';
+const data = { color: 'red'};
+
+saveFile(url, data);
+
+
+// Refactor to ES6:
+
+function saveFile(url, data) {
+  $.ajax({ url, data, method: 'POST' });
+}
+
+const url = 'http://fileupload.com';
+const data = { color: 'red'};
+
+saveFile(url, data);
+
+//================================================================
+
+
 
 
 
