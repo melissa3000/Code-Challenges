39db4b07d289947ca3e4353d0bcf99fde9539f0f
Melissa <melissaboyer3000@gmail.com>
2018-04-19 20:54:37 -0400

    Refactoring to ES6

--- practiceEs6.js
+++ practiceEs6.js
@@ -777,6 +777,44 @@ team.teamSummary();
 
 //================================================================
 
+// Refactor using ES6:
+const fibonacci = function(n) {
+  if (n < 3) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+// Solution:
+const fibonacci = (n) => {
+  if (n < 3) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+//================================================================
+
+// Understanding arrow functions and the binding of 'this'.
+// Add another key to the object below called 'getName'. It should be a
+// function that returns the profile name using this.name. Does it require
+// a fat arrow function?
+
+const profile = {
+    name: 'Alex'
+};
+
+// Solution:
+
+const profile = {
+    name: 'Alex',
+    getName: function() {
+      return this.name;
+    }
+};
+
+profile.getName();
+
+//================================================================
+
+
+
 
 
 
