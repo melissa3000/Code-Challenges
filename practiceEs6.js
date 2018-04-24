0afaa0d01bcb07ca1c7f220e39d19b10dc4f007f
Melissa <melissaboyer3000@gmail.com>
2018-04-24 08:18:17 -0400

    Destructuring

--- practiceEs6.js
+++ practiceEs6.js
@@ -1167,7 +1167,172 @@ function unshift(array, ...arrays) {
 
 
 //================================================================
+// Destructuring
+//================================================================
 
+// Refactor to ES6:
+var expense = {
+  type: 'Business',
+  amount: '$45 USD'
+};
+
+var type = expense.type;
+var amount = expense.amount;
+
+// Solution:
+
+let expense = {
+  type: 'Business',
+  amount: '$45 USD'
+};
+
+
+const { type, amount } = expense;
+
+type;
+amount;
+
+//================================================================
+
+let savedFile = {
+  extension: 'jpg',
+  name: 'repost',
+  size: 14040
+};
+
+function fileSummary(file){
+  return `The file ${file.name}.${file.extension} is of size ${file.size}`;
+}
+
+fileSummary(savedFile);
+
+// Refactor to ES6:
+
+let savedFile = {
+  extension: 'jpg',
+  name: 'repost',
+  size: 14040
+};
+
+function fileSummary({ name, extension, size }){
+  return `The file ${name}.${extension} is of size ${size}`;
+}
+
+fileSummary(savedFile);
+
+// Can also add another object as a param:
+
+let savedFile = {
+  extension: 'jpg',
+  name: 'repost',
+  size: 14040
+};
+
+function fileSummary({ name, extension, size }, { color }){
+  return `The file ${name}.${extension} is of size ${size}`;
+}
+
+fileSummary(savedFile, { color: 'red' });
+
+
+//================================================================
+
+// Destructuring arrays:
+const companies = [
+  'Google',
+  'Facebook',
+  'Uber'
+];
+
+const [ name, name2, name3, name4 ] = companies;
+name; // Google
+name2; // Facebook
+name3;  // Uber
+name4; // does not throw an error
+
+// Or:
+const [name, ... rest ] = companies;
+name; // Google
+rest; // [ 'Facebook', 'Uber']
+
+
+//================================================================
+
+// Destructuring Arrays AND Objects
+
+const companies = [
+  { name: 'Google', location: 'Mountain View' },
+  { name: 'Facebook', location: 'Menlo Park' },
+  { name: 'Uber', location: 'San Francisco' }
+];
+
+const [ location ] = companies;
+location; // {"name":"Google","location":"Mountain View"}
+
+const [{ location }] = companies;
+location; // Mountain View
+
+
+//================================================================
+
+const Google = {
+  locations: ['Mountain View', 'New York', 'London']
+};
+
+const { locations: [ location ] } = Google;
+location; // Mountain View
+
+
+//================================================================
+
+function signup(username, password, email, dob, city) {
+  // create new user
+}
+
+// tons of other code, cannot see function where you're calling it
+
+signup('myname', 'mypassword', 'myemail@example.com', '1/1/1990', 'new york');
+
+
+
+// Refactor:
+
+function signup({ username, password, email, dob, city }) {
+  // create new user
+}
+
+// tons of other code, cannot see function where you're calling it
+
+const user {
+  username: 'myname',
+  password: 'mypassword',
+  email: 'myemail@example.com',
+  dob: '1/1/1990',
+  city: 'new york'
+};
+
+signup(user);
+
+
+//================================================================
+
+const points = [
+  [4, 5],
+  [10, 1],
+  [0, 40]
+];
+
+// want to convert to this:
+[
+  { x: 4, y: 5 },
+  { x: 10, y: 1 },
+  { x: 0, y: 40 }
+]
+
+// Solution:
+points.map(([ x, y ]) => {
+  return { x, y };
+});
 
 
 
